93dd7432432066465747375bcdb251053c97db4c
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-07-13 10:08:41 -0500

    Add mad.py solution

--- 17_mad_libs/mad.py
+++ 17_mad_libs/mad.py
@@ -1,30 +1,75 @@
-﻿from typing_extensions import TypedDict
+#!/usr/bin/env python
+"""
+Purpose: An interactive game of mad libs.
+"""
+import argparse
+import re
+import sys
+from typing import List
 
 
-class Range(TypedDict):
-    min: float
-    max: float
+def get_args():
+    """Get arguments from the command line."""
+    parser = argparse.ArgumentParser(
+        description='A game of mad libs',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('file',
+                        metavar='FILE',
+                        help='File containing the mad libs text',
+                        type=argparse.FileType('rt'))
+    parser.add_argument('-i',
+                        '--inputs',
+                        type=str,
+                        nargs='*',
+                        help='Optional input arguments')
+    return parser.parse_args()
 
 
-class NutritionInformation(TypedDict):
-    value: int
-    unit: str
-    confidenceRange95Percent: Range
-    standardDeviation: float
+def find_placeholders(filename: str, text: str) -> List[str]:
+    """Parse the input file and return a list of the placeholders.
+
+    Quits if no placeholders are found.
+    """
+    if placeholders := re.findall(r'<.*?>', text):
+        return placeholders
+    print(f'"{filename}" has no placeholders.')
+    sys.exit(2)
 
 
-class RecipeNutritionInformation(TypedDict):
-    recipes_used: int
-    calories: NutritionInformation
-    fat: NutritionInformation
-    protein: NutritionInformation
-    carbs: NutritionInformation
+def get_user_inputs(placeholders: List[str]) -> List[str]:
+    """Get an input from user for each placeholder"""
+    inputs = []
+    for placeholder in placeholders:
+        inputs.append(input(f'Give me {a_or_an(placeholder)}'))
+    return inputs
 
 
-def gen_stuff() -> RecipeNutritionInformation:
-    ...
+def a_or_an(text: str) -> str:
+    """Returns the text with the proper a or an article before it."""
+    article = 'an' if text[1] in 'aeiou' else 'a'
+    return f'{article} {text[1:-1]}'
 
 
-nutrition_information: RecipeNutritionInformation = gen_stuff()
+def substitute_inputs(text: str, placeholders: List[str],
+                      inputs: List[str]) -> str:
+    """Substitute the inputs for the placeholders in the text"""
+    for placeholder, replacement in zip(placeholders, inputs):
+        text = re.sub(placeholder, replacement, text, 1)
+    return text
 
-b = nutrition_information['fat'][]
+
+def main():
+    """Get arguments and print out the mad lib."""
+    args = get_args()
+    text = args.file.read()
+    placeholders = find_placeholders(args.file.name, text)
+    if args.inputs:
+        inputs = args.inputs
+    else:
+        inputs = get_user_inputs(placeholders)
+    output = substitute_inputs(text, placeholders, inputs)
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
