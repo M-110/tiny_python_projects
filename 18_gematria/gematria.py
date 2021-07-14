7e205ff4917169399181be6243950eb786da0605
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-07-14 10:22:13 -0500

    Add gematria solution

---
+++ 18_gematria/gematria.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+Purse: Translate words into numbers.
+"""
+
+import argparse
+import re
+
+
+def get_args():
+    """Get arguments from command line."""
+    parser = argparse.ArgumentParser(
+        description='Translates words to numbers',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('text', type=str, help='Text or file to translate')
+    return parser.parse_args()
+
+
+def translate(text: str) -> str:
+    """Replace all words with their numeric translation."""
+    return re.sub(r'[^\s]+', lambda x: sum_word(x[0]), text)
+
+
+def sum_word(word: str) -> str:
+    """Sum the ordinal values of a word."""
+    return str(sum([ord(char) for char in word if char.isalnum()]))
+
+
+def main():
+    """Get arguments and print the translated words as numbers."""
+    args = get_args()
+    if '.txt' in args.text:
+        with open(args.text) as file:
+            args.text = file.read()
+    translation = translate(args.text)
+    print(translation)
+
+
+if __name__ == '__main__':
+    main()
